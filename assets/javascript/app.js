6f467ecb02891483f1e9ea6b9ab99df8485c7c64
Abraham Anderson <abeis@msn.com>
2018-03-06 16:55:49 -0600

    added BattleSystem

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,21 @@
+const BattleSystem = function (attacker, defender) {
+    // confrim attackher and defender are part of the Fighter Class
+    if(   !(attacker instanceof Fighter) ||  !(defender instanceof Fighter)  ){
+        console.error('failure to instantiate Class');
+        return
+    }
 
+    alert(`${attacker.name}  Challages ${defender.name}`);
+    console.log(defender);
+    defender.takeDammage(attacker.atack);
+    alert(`${attacker.name}  dose ${attacker.atack} dammge to ${defender.name}, ${defender.name} has ${defender.hp}Hp left `);
+    attacker.increaseAttack();
+    alert(`${attacker.name}  battle enrages and gains ${attacker.baseAttack} and now has ${attacker.atack}`);
+    attacker.takeDammage(defender.counter);
+    alert(`${defender.name} counter attacks and dose ${defender.counter} and now ${attacker.name} has ${attacker.hp}Hp left  `)
+    
+    
+}
 
 /** Figher Class */
 const data = [
@@ -6,7 +23,7 @@ const data = [
      {'name': 'Luke', 'img': 'luke.jpg', 'hp': 100, 'atack': 25,  'counter' : 5 }
 ];
 
-const fighter = function (){
+const Fighter = function (){
     this.name = null;
     this.img = null;
     this.hp = null;
@@ -15,7 +32,7 @@ const fighter = function (){
     this.counter = null;
 }
 
-fighter.prototype.create = function(fighterBio) {
+Fighter.prototype.create = function(fighterBio) {
     this.name = fighterBio.name;
     this.img = fighterBio.img;
     this.hp = fighterBio.hp;
@@ -24,13 +41,24 @@ fighter.prototype.create = function(fighterBio) {
     this.counter = fighterBio.counter;
 }
 
-fighter.prototype.increaseAttack = function() {
+Fighter.prototype.increaseAttack = function() {
     this.atack = this.atack + this.baseAttack;
 }
 
-fighter.prototype.takeDammage = function(dammge){
+Fighter.prototype.takeDammage = function(dammage){
     this.hp = this.hp - dammage;
 }
 
 
+const obi = new Fighter();
+const luke = new Fighter();
+obi.create(data[0]);
+luke.create(data[1]);
 
+
+BattleSystem(obi , luke);
+BattleSystem(obi , luke);
+BattleSystem(obi , luke);
+BattleSystem(obi , luke);
+BattleSystem(obi , luke);
+BattleSystem(obi , luke);
